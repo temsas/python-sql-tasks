7e60dab731d8dd011304d7fd9c7233d3dc476c13
temsas <artemkariakin1187@gmail.com>
2025-12-28 23:32:52 +0300

    Задание 7 выполнено

--- 2/src/solution.py
+++ 2/src/solution.py
@@ -4,5 +4,29 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def make_cars_table(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS cars (
+            id SERIAL PRIMARY KEY,
+            brand VARCHAR(50) NOT NULL,
+            model VARCHAR(50) NOT NULL
+        )
+        """)
+        conn.commit()
 
+
+def populate_cars_table(conn, cars):
+    with conn.cursor() as cursor:
+        cursor.executemany(
+            "INSERT INTO cars (brand, model) VALUES (%s, %s)",
+            cars
+        )
+        conn.commit()
+
+
+def get_all_cars(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM cars ORDER BY brand")
+        return cursor.fetchall()
 # END
--- 3/src/solution.py
+++ 3/src/solution.py
@@ -5,5 +5,17 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def batch_insert(conn, products):
+    with conn.cursor() as cursor:
+        values = [(product['name'], product['price'], product['quantity']) for product in products]
 
+        query = "INSERT INTO products (name, price, quantity) VALUES %s"
+        execute_values(cursor, query, values)
+        conn.commit()
+
+
+def get_all_products(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM products ORDER BY price DESC")
+        return cursor.fetchall()
 # END
